2322474ff9aa96941bb54441f1527fe16dd6f1cd
JonathanEKP <jonathanvaldez2001@gmail.com>
2023-07-15 22:53:40 -0600

    Ajustes de estructuras en la carpeta common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//ESTAS ESTRUCTURAS DESCRIBEN LA ESTRUCTURA DEL PRODUCTO QUE SQLX VA A RETORNAR DE LA BASE DE DATOS Y LAS RESPUESTAS DEL JSON QUE LA API PROVEERA.
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+pub struct Product{
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub price: f64,
+    pub quantity: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProductData{
+    pub product: Product,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProductResponse{
+    pub status: String,
+    pub data: ProductData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ProductListResponse{
+    pub status: String,
+    pub result: i32,
+    pub product: Vec<Product>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ErrorResponse{
+    pub status: String,
+    pub message: String,
 }
